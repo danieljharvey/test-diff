f643e7af467b295e9b33c792fc49465978dceee9
Daniel J. Harvey <danieljamesharvey@gmail.com>
2018-10-01 20:24:19 +0100

    What?

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ const { exec } = require('child_process');
 
 const gitCommand = `git diff --no-commit-id --name-only -r \`git log -n 2 --oneline --pretty=format:"%h"\` | sort -u`
 
+function execPromise(command) {
+    return new Promise((resolve, reject) => {
+        exec(gitCommand, (err, stdout, stderr) => {
+            if (err) {
+              // node couldn't execute the command
+              return reject(err)
+            }
+            return resolve(stdout)
+        });
+    })
+}
 
-exec(gitCommand, (err, stdout, stderr) => {
-  if (err) {
-    // node couldn't execute the command
-    return;
-  }
-
-  // the *entire* stdout and stderr (buffered)
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-});
+execPromise(gitCommand).then(console.log).catch(console.error)
